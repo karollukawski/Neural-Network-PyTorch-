c373b13bc3f0b6265c1d3aeaac15a9e030ab2419
karollukawski <83611706+karollukawski@users.noreply.github.com>
2022-07-27 20:39:26 +0200

    Added comments

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import itertools
-
 from sklearn.datasets import make_classification
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
@@ -19,18 +18,19 @@ import seaborn as sns
 
 X, y = make_classification(n_samples=10000, n_features=4, n_classes=3, n_redundant=0, n_informative=3, n_clusters_per_class=2)
 
-# #Visualizing the dataset
-#
-# plt.title("Multi-class data, 4 informative features, 3 classes", fontsize="large")
-# plt.scatter(X[:, 0], X[:, 1], marker="o", c=Y, s=25, edgecolor="k")
-#
-# plt.show()
-#
+#Visualizing the dataset
+
+plt.title("Multi-class data, 4 informative features, 3 classes", fontsize="large")
+plt.scatter(X[:, 0], X[:, 1], marker="o", c=y, s=25, edgecolor="k")
+
+plt.show()
+
 #Spliting the dataset into test and train sets
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=26)
 
-# Visualize the data.
+# Visualizing the data
+
 fig, (train_ax, test_ax) = plt.subplots(ncols=2, sharex=True, sharey=True, figsize=(10, 5))
 train_ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=plt.cm.Spectral)
 train_ax.set_title("Training Data")
@@ -42,7 +42,8 @@ test_ax.set_xlabel("Feature #0")
 test_ax.set_title("Testing data")
 plt.show()
 
-# Convert data to torch tensors
+# Converting data to torch tensors
+
 class Data(Dataset):
     def __init__(self, X, y):
         self.X = torch.from_numpy(X.astype(np.float32))
@@ -57,14 +58,15 @@ class Data(Dataset):
 
 batch_size = 2
 
-# Instantiate training and test data
+# Instantiating training and testing data
 train_data = Data(X_train, y_train)
 train_dataloader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=True)
 
 test_data = Data(X_test, y_test)
 test_dataloader = DataLoader(dataset=test_data, batch_size=batch_size, shuffle=True)
 
-# Check it's working
+# Checking working
+
 for batch, (X, y) in enumerate(train_dataloader):
     print(f"Batch: {batch+1}")
     print(f"X shape: {X.shape}")
@@ -73,10 +75,13 @@ for batch, (X, y) in enumerate(train_dataloader):
 
 print("batch complete")
 
+#Defining the dimensions of the network
+
 input_dim = 4
 hidden_dim = 2
 output_dim = 1
 
+#Defining the neural network class
 
 class NeuralNetwork(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
@@ -91,9 +96,12 @@ class NeuralNetwork(nn.Module):
 
         return x
 
+#Instantiating the classifier
 
 model = NeuralNetwork(input_dim, hidden_dim, output_dim)
 print(model)
+
+#Training model
 
 learning_rate = 0.1
 
@@ -106,10 +114,10 @@ loss_values = []
 
 for epoch in range(num_epochs):
     for X, y in train_dataloader:
-# zero the parameter gradients
+        # zero the parameter gradients
         optimizer.zero_grad()
 
-# forward + backward + optimize
+        # forward + backward + optimize
         pred = model(X)
         loss = loss_fn(pred, y.unsqueeze(-1))
         loss_values.append(loss.item())
@@ -117,6 +125,8 @@ for epoch in range(num_epochs):
         optimizer.step()
 
 print("Training Complete")
+
+#134 sth wrong
 
 step = np.linspace(0, 100, 10500)
 
@@ -159,26 +169,31 @@ sns.heatmap(cf_matrix, annot=True, cbar=False, fmt="g")
 plt.show()
 
 
-###########################################
+############################
+#Additional functions to use
+############################
+
+#Sigmoid function
+#
 # x = np.linspace(-5, 5, 50)
 # z = 1/(1 + np.exp(-x))
-#
-#
-#
 #
 # plt.subplots(figsize=(8, 5))
 # plt.plot(x, z)
 # plt.grid()
 # plt.show()
+#
+#Tanh function (hyperbolic tangent)
 #
 # x = np.linspace(-5, 5, 50)
 # z = np.tanh(x)
 #
-#
 # plt.subplots(figsize=(8, 5))
 # plt.plot(x, z)
 # plt.grid()
 # plt.show()
+#
+#Rectified Linear Unit (ReLU)
 #
 # x = np.linspace(-5, 5, 50)
 # z = [max(0, i) for i in x]
